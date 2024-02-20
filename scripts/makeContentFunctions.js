fc054c92191f0c86ac07e8d0b0e92e2c9f1bbcf9
ladn0 <84666429+kodika-dangerlyoha@users.noreply.github.com>
2024-02-20 17:47:07 +0300

    n4.11
    -> общий css style для горизонтальных позиций игр
    game card:
    -> заполнение через js похожих игр

--- scripts/makeContentFunctions.js
+++ scripts/makeContentFunctions.js
@@ -1,3 +1,16 @@
+function getGames() {
+    return games;
+}
+
+function getGames_for_salesHits() {
+    return games.slice(0, 5);
+}
+
+function getGames_similar() {
+    return games.slice(0, 6);
+}
+
+
 function makeOffers_index() {
     let div = document.querySelector('#offers');
     div.innerHTML = "";
@@ -5,33 +18,33 @@ function makeOffers_index() {
     function addGame() {
         let grid = document.createElement('div');
         
-        games.forEach(game => {
+        getGames().forEach(game => {
             let discount = Math.round((game.oldPrice - game.newPrice) / game.oldPrice * 100);
-            let blockSiteHTML = `<div class="offers__grid__game__banner">
+            let blockSiteHTML = `<div class="gridHorizontalGames__game__banner">
                                     <img src="${game.imgH}" alt="">
-                                    <div class="offers__grid__game__banner__videoBlock">
+                                    <div class="gridHorizontalGames__game__banner__videoBlock">
                                         <video src="${game.treilerSrc}" autoplay loop muted>
                                     </div>   
-                                    <div class="offers__grid__game__banner__discount">
-                                        <div class="offers__grid__game__banner__discount__inner txt">${discount}%</div>
+                                    <div class="gridHorizontalGames__game__banner__discount">
+                                        <div class="gridHorizontalGames__game__banner__discount__inner txt">${discount}%</div>
                                     </div>
                                 </div>
-                                <div class="offers__grid__game__info">
-                                    <div class="offers__grid__game__info__buttons">
-                                        <div class="offers__grid__game__info__buttons__button">
-                                            <div class="offers__grid__game__info__buttons__button__forHover offers__grid__game__info__buttons__button__forHover_blue absolute-zero"></div>
+                                <div class="gridHorizontalGames__game__info">
+                                    <div class="gridHorizontalGames__game__info__buttons">
+                                        <div class="gridHorizontalGames__game__info__buttons__button">
+                                            <div class="gridHorizontalGames__game__info__buttons__button__forHover gridHorizontalGames__game__info__buttons__button__forHover_blue absolute-zero"></div>
                                             <img src="img/icons/basket64.png" alt="">
                                         </div>
-                                        <div class="offers__grid__game__info__buttons__button">
-                                            <div class="offers__grid__game__info__buttons__button__forHover offers__grid__game__info__buttons__button__forHover_red absolute-zero"></div>
+                                        <div class="gridHorizontalGames__game__info__buttons__button">
+                                            <div class="gridHorizontalGames__game__info__buttons__button__forHover gridHorizontalGames__game__info__buttons__button__forHover_red absolute-zero"></div>
                                             <img src="img/icons/like64.png" alt="">
                                         </div>
                                     </div>
-                                    <div class="offers__grid__game__info__name txt">${game.name}</div>
-                                    <div class="offers__grid__game__info__price txt">${game.newPrice}₽</div>
+                                    <div class="gridHorizontalGames__game__info__name txt">${game.name}</div>
+                                    <div class="gridHorizontalGames__game__info__price txt">${game.newPrice}₽</div>
                                 </div>`;
             let div = document.createElement('div');
-            div.classList.add("offers__grid__game");
+            div.classList.add("gridHorizontalGames__game");
             div.innerHTML = blockSiteHTML;
             grid.append(div);
         });
@@ -52,7 +65,7 @@ function makeOffers_index() {
                                 <div class="offers__head__nav__strip"></div>
                             </div>
                         </div>
-                        <div class="offers__grid">
+                        <div class="offers__grid gridHorizontalGames">
                             ${addGame()}
                         </div>
                         <div class="offers__blockButtonMore">
@@ -65,6 +78,54 @@ function makeOffers_index() {
     div.innerHTML = blockSiteHTML;
 }
 
+function makeSimilar_gameCard() {
+    let div = document.querySelector('#similar');
+    div.innerHTML = "";
+
+    function addGame() {
+        let grid = document.createElement('div');
+        
+        getGames_similar().forEach(game => {
+            let discount = Math.round((game.oldPrice - game.newPrice) / game.oldPrice * 100);
+            let blockSiteHTML = `<div class="gridHorizontalGames__game__banner">
+                                    <img src="${game.imgH}" alt="">
+                                    <div class="gridHorizontalGames__game__banner__videoBlock">
+                                        <video src="${game.treilerSrc}" autoplay loop muted>
+                                    </div>   
+                                    <div class="gridHorizontalGames__game__banner__discount">
+                                        <div class="gridHorizontalGames__game__banner__discount__inner txt">${discount}%</div>
+                                    </div>
+                                </div>
+                                <div class="gridHorizontalGames__game__info">
+                                    <div class="gridHorizontalGames__game__info__buttons">
+                                        <div class="gridHorizontalGames__game__info__buttons__button">
+                                            <div class="gridHorizontalGames__game__info__buttons__button__forHover gridHorizontalGames__game__info__buttons__button__forHover_blue absolute-zero"></div>
+                                            <img src="img/icons/basket64.png" alt="">
+                                        </div>
+                                        <div class="gridHorizontalGames__game__info__buttons__button">
+                                            <div class="gridHorizontalGames__game__info__buttons__button__forHover gridHorizontalGames__game__info__buttons__button__forHover_red absolute-zero"></div>
+                                            <img src="img/icons/like64.png" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="gridHorizontalGames__game__info__name txt">${game.name}</div>
+                                    <div class="gridHorizontalGames__game__info__price txt">${game.newPrice}₽</div>
+                                </div>`;
+            let div = document.createElement('div');
+            div.classList.add("gridHorizontalGames__game");
+            div.innerHTML = blockSiteHTML;
+            grid.append(div);
+        });
+        return grid.innerHTML;
+    }
+
+    let blockSiteHTML = `<h3 class="txt">Похожие игры</h3>
+                        <div class="similar__grid gridHorizontalGames">
+                            ${addGame()}
+                        </div>`;
+
+    div.innerHTML = blockSiteHTML;
+}
+
 function makeSalesHits_index() {
     let div = document.querySelector('#salesHits');
     div.innerHTML = "";
@@ -72,8 +133,7 @@ function makeSalesHits_index() {
     function addGame() {
         let grid = document.createElement('div');
         
-        games.forEach(game => {
-            if (game.id < 5) {
+        getGames_for_salesHits().forEach(game => {
                 let discount = Math.round((game.oldPrice - game.newPrice) / game.oldPrice * 100);
                 let blockSiteHTML = `<div class="salesHits__games__game__banner">
                                         <img src="${game.imgW}" alt="">
@@ -99,7 +159,6 @@ function makeSalesHits_index() {
                 div.classList.add("salesHits__games__game");
                 div.innerHTML = blockSiteHTML;
                 grid.append(div);
-            }
         });
         return grid.innerHTML;
     }
@@ -126,7 +185,7 @@ function makeCatalog_catalog() {
     function addGame() {
         let grid = document.createElement('div');
         
-        games.forEach(game => {
+        getGames().forEach(game => {
                 let discount = Math.round((game.oldPrice - game.newPrice) / game.oldPrice * 100);
                 let blockSiteHTML = ` <div class="catalog__grid__game__banner">
                                             <img src="${game.imgW}" alt="">
@@ -171,7 +230,7 @@ function makeFavorite_favorite() {
     function addGame() {
         let grid = document.createElement('div');
         
-        games.forEach(game => {
+        getGames().forEach(game => {
                 let discount = Math.round((game.oldPrice - game.newPrice) / game.oldPrice * 100);
                 let blockSiteHTML = ` <div class="catalog__grid__game__banner">
                                             <img src="${game.imgW}" alt="">
